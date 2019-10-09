5afc36f31485626e109ea3abc64a23f9898f4eb0
Daniel Hoffmann <oralordos@gmail.com>
2019-10-09 12:22:15 -0700

    Base64 user:pass example

--- main.go
+++ main.go
@@ -1,47 +1,10 @@
 package main
 
 import (
-	"encoding/json"
+	"encoding/base64"
 	"fmt"
-	"log"
-	"net/http"
 )
 
-type person struct {
-	First string
-}
-
 func main() {
-	http.HandleFunc("/encode", foo)
-	http.HandleFunc("/decode", bar)
-	http.ListenAndServe(":8080", nil)
-}
-
-func foo(w http.ResponseWriter, r *http.Request) {
-	p1 := person{
-		First: "Jenny",
-	}
-
-	p2 := person{
-		First: "James",
-	}
-
-	people := []person{p1, p2}
-
-	err := json.NewEncoder(w).Encode(people)
-	if err != nil {
-		log.Println("Bad data to encode", err)
-	}
-}
-
-func bar(w http.ResponseWriter, r *http.Request) {
-
-	people := []person{}
-
-	err := json.NewDecoder(r.Body).Decode(&people)
-	if err != nil {
-		log.Println("Bad data to decode", err)
-	}
-
-	fmt.Println(people)
+	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
 }
