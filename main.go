3684f9f3ad373c836baf68fd226f1e548e3381b5
Daniel Hoffmann <oralordos@gmail.com>
2019-10-09 12:38:52 -0700

    Hashing example

--- main.go
+++ main.go
@@ -1,10 +1,40 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
+	pass := "123456789"
+
+	hashedPass, err := hashPassword(pass)
+	if err != nil {
+		panic(err)
+	}
+
+	err = comparePassword(pass, hashedPass)
+	if err != nil {
+		log.Fatalln("Not logged in")
+	}
+
+	log.Println("Logged in!")
+}
+
+func hashPassword(password string) ([]byte, error) {
+	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("Error while generating bcrypt hash from password: %w", err)
+	}
+	return bs, nil
+}
+
+func comparePassword(password string, hashedPass []byte) error {
+	err := bcrypt.CompareHashAndPassword(hashedPass, []byte(password))
+	if err != nil {
+		return fmt.Errorf("Invalid password: %w", err)
+	}
+	return nil
 }
