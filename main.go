47b8aba92ed2d9fe242a51643225ba924be321f3
Daniel Hoffmann <oralordos@gmail.com>
2019-10-10 15:04:18 -0700

    Create claims

--- main.go
+++ main.go
@@ -5,9 +5,28 @@ import (
 	"crypto/sha512"
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
+
+type UserClaims struct {
+	jwt.StandardClaims
+	SessionID int64
+}
+
+func (u *UserClaims) Valid() error {
+	if !u.VerifyExpiresAt(time.Now().Unix(), true) {
+		return fmt.Errorf("Token has expired")
+	}
+
+	if u.SessionID == 0 {
+		return fmt.Errorf("Invalid session ID")
+	}
+
+	return nil
+}
 
 var key = []byte{}
 
