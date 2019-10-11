cda4ef10203b1010356ea89d43715db1d283c032
Daniel Hoffmann <oralordos@gmail.com>
2019-10-11 15:40:51 -0700

    AES example

--- main.go
+++ main.go
@@ -1,153 +1,45 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/sha512"
-	"crypto/rand"
-	"io"
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 	"fmt"
-	"log"
-	"time"
-
-	"github.com/dgrijalva/jwt-go"
-	"golang.org/x/crypto/bcrypt"
-	"github.com/gofrs/uuid"
 )
 
-type UserClaims struct {
-	jwt.StandardClaims
-	SessionID int64
-}
-
-func (u *UserClaims) Valid() error {
-	if !u.VerifyExpiresAt(time.Now().Unix(), true) {
-		return fmt.Errorf("Token has expired")
-	}
-
-	if u.SessionID == 0 {
-		return fmt.Errorf("Invalid session ID")
-	}
-
-	return nil
-}
-
 func main() {
-	pass := "123456789"
-
-	hashedPass, err := hashPassword(pass)
+	key := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+	encrypted, err := encrypt([]byte("Hello World!"), key)
 	if err != nil {
 		panic(err)
 	}
 
-	err = comparePassword(pass, hashedPass)
-	if err != nil {
-		log.Fatalln("Not logged in")
-	}
+	fmt.Println(string(encrypted))
 
-	log.Println("Logged in!")
+	decrypted, err := encrypt(encrypted, key)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(decrypted))
 }
 
-func hashPassword(password string) ([]byte, error) {
-	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+func encrypt(message, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
 	if err != nil {
-		return nil, fmt.Errorf("Error while generating bcrypt hash from password: %w", err)
+		return nil, fmt.Errorf("Error in encryption: %w", err)
 	}
-	return bs, nil
-}
 
-func comparePassword(password string, hashedPass []byte) error {
-	err := bcrypt.CompareHashAndPassword(hashedPass, []byte(password))
+	stream := cipher.NewOFB(block, make([]byte, aes.BlockSize))
+	buf := &bytes.Buffer{}
+	wtr := cipher.StreamWriter{
+		S: stream,
+		W: buf,
+	}
+
+	_, err = wtr.Write(message)
 	if err != nil {
-		return fmt.Errorf("Invalid password: %w", err)
-	}
-	return nil
-}
-
-func signMessage(msg []byte) ([]byte, error) {
-	h := hmac.New(sha512.New, keys[currentKid].key)
-
-	_, err := h.Write(msg)
-	if err != nil {
-		return nil, fmt.Errorf("Error in signMessage while hashing message: %w", err)
+		return nil, fmt.Errorf("Error in encryption: %w", err)
 	}
 
-	signature := h.Sum(nil)
-	return signature, nil
-}
-
-func checkSig(msg, sig []byte) (bool, error) {
-	newSig, err := signMessage(msg)
-	if err != nil {
-		return false, fmt.Errorf("Error in checkSig while getting signature of message: %w", err)
-	}
-
-	same := hmac.Equal(newSig, sig)
-	return same, nil
-}
-
-func createToken(c *UserClaims) (string, error) {
-	t := jwt.NewWithClaims(jwt.SigningMethodHS512, c)
-	signedToken, err := t.SignedString(keys[currentKid].key)
-	if err != nil {
-		return "", fmt.Errorf("Error in createToken when signing token: %w", err)
-	}
-	return signedToken, nil
-}
-
-func generateNewKey() error {
-	newKey := make([]byte, 64)
-	_, err := io.ReadFull(rand.Reader, newKey)
-	if err != nil {
-		return fmt.Errorf("Error in generateNewKey while generating key: %w", err)
-	}
-
-	uid, err := uuid.NewV4()
-	if err != nil {
-		return fmt.Errorf("Error in generateNewKey while generating kid: %w", err)
-	}
-
-	keys[uid.String()] = key{
-		key: newKey,
-		created: time.Now(),
-	}
-	currentKid = uid.String()
-
-	return nil
-}
-
-type key struct {
-	key []byte
-	created time.Time
-}
-
-var currentKid = ""
-var keys = map[string]key{}
-
-func parseToken(signedToken string) (*UserClaims, error) {
-	t, err := jwt.ParseWithClaims(signedToken, &UserClaims{}, func (t *jwt.Token) (interface{}, error) {
-		if t.Method.Alg() != jwt.SigningMethodHS512.Alg() {
-			return nil, fmt.Errorf("Invalid signing algorithm")
-		}
-
-		kid, ok := t.Header["kid"].(string)
-		if !ok {
-			return nil, fmt.Errorf("Invalid key ID")
-		}
-
-		k, ok := keys[kid]
-		if !ok {
-			return nil, fmt.Errorf("Invalid key ID")
-		}
-
-		return k.key, nil
-	})
-	if err != nil {
-		return nil, fmt.Errorf("Error in parseToken while parsing token: %w", err)
-	}
-
-	if !t.Valid {
-		return nil, fmt.Errorf("Error in parseToken, token is not valid")
-	}
-
-	return t.Claims.(*UserClaims), nil
+	return buf.Bytes(), err
 }
