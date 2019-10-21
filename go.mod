b74eb6646c81b23ce2f1ec9848806ff9762a4c77
Todd McLeod <tuddleymc@gmail.com>
2019-10-21 12:18:53 -0700

    bringing up main.go from ninja 02 v0.32.0

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.13
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/google/uuid v1.1.1
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3 // indirect
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
 )
--- main.go
+++ main.go
@@ -2,99 +2,215 @@ package main
 
 import (
 	"fmt"
-	"encoding/json"
+	"log"
 	"net/http"
-	"strings"
+	"net/url"
 
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/github"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
-// Key is github ID, value is user ID
-var githubConnections map[string]string
-
-// JSON layout: {"data":{"viewer":{"id":"..."}}}
-type githubResponse struct {
-	Data struct{
-		Viewer struct {
-			ID string `json:"id"`
-		} `json:"viewer"`
-	} `json:"data"`
+type user struct {
+	password []byte
+	First    string
 }
 
-var githubOauthConfig = &oauth2.Config{
-	ClientID:     "616045a39889744799a7",
-	ClientSecret: "c04f7a269768cd87bfb0c9a6b329a49a566c3707",
-	Endpoint:     github.Endpoint,
-}
+// key is email, value is user
+var db = map[string]user{}
+
+// key is sessionid, value is email
+var sessions = map[string]string{}
 
 func main() {
 	http.HandleFunc("/", index)
-	http.HandleFunc("/oauth/github", startGithubOauth)
-	http.HandleFunc("/oauth2/receive", completeGithubOauth)
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, `<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	<form action="/oauth/github" method="post">
-		<input type="submit" value="Login with Github">
+	c, err := r.Cookie("sessionID")
+	if err != nil {
+		c = &http.Cookie{
+			Name:  "sessionID",
+			Value: "",
+		}
+	}
+
+	sID, err := parseToken(c.Value)
+	if err != nil {
+		log.Println("index parseToken", err)
+	}
+
+	var e string
+	if sID != "" {
+		e = sessions[sID]
+	}
+
+	var f string
+	if user, ok := db[e]; ok {
+		f = user.First
+	}
+
+	errMsg := r.FormValue("msg")
+
+	fmt.Fprintf(w, `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Document</title>
+	</head>
+	<body>
+	<h1>IF YOU HAVE A SESSION, HERE IS YOUR NAME: %s</h1>
+	<h1>IF YOU HAVE A SESSION, HERE IS YOUR EMAIL: %s</h1>
+	<h1>IF THERE IS ANY MESSAGE FOR YOU, HERE IT IS: %s</h1>
+        <h1>REGISTER</h1>
+		<form action="/register" method="POST">
+		<label for="first">First</label>
+		<input type="text" name="first" placeholder="First" id="first">
+		<input type="email" name="e">
+			<input type="password" name="p">
+			<input type="submit">
+        </form>
+        <h1>LOG IN</h1>
+        <form action="/login" method="POST">
+            <input type="email" name="e">
+			<input type="password" name="p">
+			<input type="submit">
+		</form>
+		<h1>LOGOUT</h1>
+		<form action="/logout" method="POST">
+		<input type="submit" value="LOGOUT">
 	</form>
-</body>
-</html>`)
+	</body>
+	</html>`, f, e, errMsg)
 }
 
-func startGithubOauth(w http.ResponseWriter, r *http.Request) {
-	redirectURL := githubOauthConfig.AuthCodeURL("0000")
-	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+func register(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		msg := url.QueryEscape("your method was not post")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	e := r.FormValue("e")
+	if e == "" {
+		msg := url.QueryEscape("your email needs to not be empty")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	p := r.FormValue("p")
+	if p == "" {
+		msg := url.QueryEscape("your email password needs to not be empty")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	f := r.FormValue("first")
+	if f == "" {
+		msg := url.QueryEscape("your first name needs to not be empty")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	bsp, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.DefaultCost)
+	if err != nil {
+		msg := "there was an internal server error - evil laugh: hahahahaha"
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+	log.Println("password", p)
+	log.Println("bcrypted", bsp)
+	db[e] = user{
+		password: bsp,
+		First:    f,
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
-func completeGithubOauth(w http.ResponseWriter, r *http.Request) {
-	code := r.FormValue("code")
-	state := r.FormValue("state")
-
-	if state != "0000" {
-		http.Error(w, "State is incorrect", http.StatusBadRequest)
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		msg := url.QueryEscape("your method was not post")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
 		return
 	}
 
-	token, err := githubOauthConfig.Exchange(r.Context(), code)
+	e := r.FormValue("e")
+	if e == "" {
+		msg := url.QueryEscape("your email needs to not be empty")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	p := r.FormValue("p")
+	if p == "" {
+		msg := url.QueryEscape("your email password needs to not be empty")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	if _, ok := db[e]; !ok {
+		msg := url.QueryEscape("your email or password didn't match")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword(db[e].password, []byte(p))
 	if err != nil {
-		http.Error(w, "Couldn't login", http.StatusInternalServerError)
+		msg := url.QueryEscape("your email or password didn't match")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
 		return
 	}
 
-	ts := githubOauthConfig.TokenSource(r.Context(), token)
-	client := oauth2.NewClient(r.Context(), ts)
-
-	requestBody := strings.NewReader(`{"query": "query {viewer {id}}"}`)
-	resp, err := client.Post("https://api.github.com/graphql", "application/json", requestBody)
+	sUUID := uuid.New().String()
+	sessions[sUUID] = e
+	token, err := createToken(sUUID)
 	if err != nil {
-		http.Error(w, "Couldn't get user", http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
-
-	var gr githubResponse
-	err = json.NewDecoder(resp.Body).Decode(&gr)
-	if err != nil {
-		http.Error(w, "Github invalid response", http.StatusInternalServerError)
+		log.Println("couldn't createToken in login", err)
+		msg := url.QueryEscape("our server didn't get enough lunch and is not working 200% right now. Try bak later")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
 		return
 	}
 
-	githubID := gr.Data.Viewer.ID
-	userID, ok := githubConnections[githubID]
-	if !ok {
-		// New User - create account
-		// Maybe return, maybe not, depends
+	c := http.Cookie{
+		Name:  "sessionID",
+		Value: token,
 	}
 
-	// Login to account userID using JWT
+	http.SetCookie(w, &c)
+
+	msg := url.QueryEscape("you logged in " + e)
+	http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
 }
 
+func logout(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	c, err := r.Cookie("sessionID")
+	if err != nil {
+		c = &http.Cookie{
+			Name:  "sessionID",
+			Value: "",
+		}
+	}
+
+	sID, err := parseToken(c.Value)
+	if err != nil {
+		log.Println("index parseToken", err)
+	}
+
+	delete(sessions, sID)
+
+	c.MaxAge = -1
+
+	http.SetCookie(w, c)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
